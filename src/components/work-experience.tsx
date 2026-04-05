cff707397ff47f0ef471eb2d6a556db2b3f211c8
wing_shiva <shivatirupathi10@gmail.com>
2026-04-05 12:33:18 +1000

    updated styles on iffa and keka work experience

--- src/components/work-experience.tsx
+++ src/components/work-experience.tsx
@@ -12,17 +12,17 @@ function WorkExperience() {
 
                     {/* Work Experience at IFFA */}
                     <div className="flex p-6 gap-4">
-                        <div className="box-shadow-black-000000 h-46 w-36 p-4 flex flex-col gap-2 border rounded-3xl bg-[#ffffff] min-w-[144px]">
-                            <div className="h-1/3">
+                        <div className="box-shadow-black-000000 h-46 w-36 p-4 flex flex-col gap-2 border rounded-3xl bg-[#ebd885] min-w-[144px]">
+                            <div className="h-1/3 rounded-xl overflow-hidden">
                                 <img className="w-full h-full" src={iffaPng}></img>
                             </div>
                             <div className="flex flex-grow flex-col justify-between">
                                 <p className="leading-none text-center text-sm">IT - Developer </p>
-                                <button onClick={() => { window.open("https://www.iffa.com.au/", '_blank') }} className="transition ease-in-out duration-300 box-shadow-black-000000 bg-[#ffffff] cursor-pointer rounded-xl text-sm border-[1.5px] py-[2px]">visit</button>
+                                <button onClick={() => { window.open("https://www.iffa.com.au/", '_blank') }} className="transition text-[#ebd885] ease-in-out duration-300 box-shadow-black-000000 bg-[#4d3d22] border-none cursor-pointer rounded-xl text-sm border-[1.5px] py-[2px]">Visit Company</button>
                             </div>
                         </div>
                         <div className="">
-                            <h1 className="font-semibold">International Film Festival of Australia<span className="text-xs">Melbourne - (Mar 2025 - Jun 2025) - Internship</span></h1>
+                            <h1 className="font-semibold">International Film Festival of Australia<span className="text-xs"> - Melbourne - (Mar 2025 - Jun 2025) - Internship</span></h1>
                             <ul className="list-disc pl-6 text-sm">
                                 <li>
                                     Streamlined development workflow by moving all static data to service files and created shared components, increasing
@@ -49,12 +49,12 @@ function WorkExperience() {
                             </div>
                             <div className="flex flex-grow flex-col justify-between">
                                 <p className="leading-none text-center text-sm">Associate Software Engineer</p>
-                                <button onClick={() => { window.open("https://www.keka.com/", '_blank') }} className="transition ease-in-out duration-300 box-shadow-black-000000 bg-[#ffffff] cursor-pointer rounded-xl text-sm border-[1.5px] py-[2px]">visit</button>
+                                <button onClick={() => { window.open("https://www.keka.com/", '_blank') }} className="transition ease-in-out duration-300 box-shadow-black-000000 bg-[#ffffff] cursor-pointer rounded-xl text-sm border-[1.5px] py-[2px]">Visit Company</button>
                             </div>
                         </div>
                         <div className="">
                             <h1 className="font-semibold">KEKA Technologies <span className="text-xs">
-                                Hyderabad - (May 2023 - Dec 2024)</span></h1>
+                                - Hyderabad - (May 2023 - Dec 2024) - Full Time</span></h1>
                             <ul className="list-disc pl-6 text-sm">
                                 <li>
                                     Played a pivotal role in developing the KEKA Marketplace standalone application from scratch by architecting foundational
